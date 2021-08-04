350efc941cc4f433086e731fbc3a1e56235b9031
gamaray123 <83606175+gamaray123@users.noreply.github.com>
2021-08-04 13:51:23 +0000

    Add files via upload

---
+++ data_link.php
@@ -0,0 +1,25 @@
+
+<h1> RESULTS <h1>
+<br>
+<?php
+$servername = "localhost";
+$username = "hamakins";
+$password = "password123";
+$dbname = "Users";
+$id = $_GET["id"];
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT id, User FROM Users WHERE id = " . $id;
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    echo "Id: " . $row["id"]. "        Username: " . $row["User"] . "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
